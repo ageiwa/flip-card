0afda0f978b016c5acb96dae381310fb3e137ed4
vlad ageysha <ander123fakk@gmail.com>
2022-07-10 17:47:07 +0300

    I started writing a script for placing cards

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('helo')
+const playingField = document.querySelector('.playing-field');
+const btnStart = document.querySelector('#btn-start');
+
+btnStart.addEventListener('click', () => {
+    for (let i = 0; i < 12; i++) {
+        const card = document.createElement('div');
+        card.className = 'card';
+        playingField.appendChild(card);
+    }
+
+
+});
